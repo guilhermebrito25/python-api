98ba4ac29c61f3fff979755fa4513c9139937bf1
Guilherme Brito <94586533+guilhermebrito25@users.noreply.github.com>
2024-09-30 06:00:40 -0400

    att
    -conexão bd - slqalchemy
    -criação tabela usuarios no bd

--- app.py
+++ app.py
@@ -1,24 +1,28 @@
 from flask import Flask, jsonify, request
 
+from sqlalchemy import create_engine, String
+from sqlalchemy.orm import declarative_base, Mapped, mapped_column
+
+
+##CRIAÇÃO FUNÇÃO CONEXÃO COM O BD - ALCHEMYSQL
+engine = create_engine("mysql+mysqldb://root:1234@localhost:3306/crm2", echo=True)
+
+
+##CRIANDO AS COLUNAS NO BANCO DE DADOS - 
+base = declarative_base()
+class Usuario(base): #classe criadora "base declarativa" - 
+    __tablename__ = 'usuarios' #nome da tabela
+    
+    id: Mapped[int] = mapped_column(primary_key=True) #OBRIGATORIO - 
+    name: Mapped[str] = mapped_column(String(30))
+    login: Mapped[str] = mapped_column(String(20))
+    password: Mapped[str] = mapped_column(String(20))
+base.metadata.create_all(engine) #CRIAÇÃO LITERAL DAS TABELAS NO BANCO
+
+
+##CRIAÇÃO FUNÇÃO APP DO FLASK
 app = Flask(__name__)
 
-livros = [
-    {   
-        'id': 1,
-        'livro': 'Pedra filosofal',
-        'autor': 'Kleber BamBam'
-    },
-    {
-        'id': 2,
-        'livro': 'Uma estrela no ceu',
-        'autor': 'JuniorBaiano'
-    },
-    {   
-        'id': 3,
-        'livro': 'O menino emdemoiado',
-        'autor': 'Popó mão de ferro'
-    }
-]
 
 #Pega todos os livros - 
 @app.route('/livros', methods=['GET'])

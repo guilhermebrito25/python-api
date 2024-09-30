34dd1453fa2ca05724f1476a53f91ebd0cdcf541
Guilherme Brito <94586533+guilhermebrito25@users.noreply.github.com>
2024-09-30 17:07:50 -0400

    renovation-coding-v.2.0.00.0
    Foi reestrurado o código novamente, com metodos diferentes no slqAlchemy

--- app.py
+++ app.py
@@ -1,53 +1,85 @@
+
 from flask import Flask, jsonify, request
+from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-from sqlalchemy import create_engine, String
-from sqlalchemy.orm import declarative_base, Mapped, mapped_column
-
-
-##CRIAÇÃO FUNÇÃO CONEXÃO COM O BD - ALCHEMYSQL
-engine = create_engine("mysql+mysqldb://root:1234@localhost:3306/crm2", echo=True)
-
-
-##CRIANDO AS COLUNAS NO BANCO DE DADOS - 
-base = declarative_base()
-class Usuario(base): #classe criadora "base declarativa" - 
-    __tablename__ = 'usuarios' #nome da tabela
-    
-    id: Mapped[int] = mapped_column(primary_key=True) #OBRIGATORIO - 
-    name: Mapped[str] = mapped_column(String(30))
-    login: Mapped[str] = mapped_column(String(20))
-    password: Mapped[str] = mapped_column(String(20))
-base.metadata.create_all(engine) #CRIAÇÃO LITERAL DAS TABELAS NO BANCO
-
-
-##CRIAÇÃO FUNÇÃO APP DO FLASK
+#cria o router do flask
 app = Flask(__name__)
 
+#conecta o alchemy no BD
+engine = create_engine("mysql+mysqldb://root:1234@localhost:3306/crm3", echo=True)
 
-#Pega todos os livros - 
-@app.route('/livros', methods=['GET'])
-def pegarLivros():
-    return jsonify(livros)
+#inicia seção, onde será usado os metodos CRUD
+Session = sessionmaker(bind=engine)
+session = Session()
 
-#Pega um livro por ID
-@app.route('/livros/<int:id>', methods=['GET'])
-def pegarLivroId(id):
-    for c in livros:
-        if c.get('id') == id:
-            return jsonify(c)
 
-#Edita um livro por ID
-@app.route('/livros/<int:id>', methods=['PUT'])
-def editaLivroId(id):
-    livroAlterado = request.get_json()
-    for i,l in enumerate(livros):
-        if l.get('id') == id:
-            livros[i].update(livroAlterado)
-            return jsonify(livros[i])
-        
+#inicia a "schame" e criação da estrura do BD com as tabelas(usuarios e livros)
+Base = declarative_base()
 
+#usuarios
+class Usuario(Base):
+    __tablename__= 'usuarios'
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    nome = Column("nome", String(50))
+    login = Column("login", String(20))
+    password = Column("password", String(20))
+    
+    def __init__(self, nome, login, password):
+        self.nome = nome
+        self.login = login
+        self.password = password
+
+#livros
+class Livro(Base):
+    __tablename__= 'livros'
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    titulo = Column("titulo", String(50))
+    autor = Column("autor", String(50))
+    resumo = Column("resumo", String(150))
+
+    def __init__(self, titulo, autor, resumo):
+        self.titulo = titulo
+        self.autor = autor
+        self.resumo = resumo
+    
+Base.metadata.create_all(bind=engine)
+
+
+#CRUD
+#CRIAR
+@app.route('/usuario/add', methods=['POST'])
+def criar_usuario():
+    dados_usuario = request.get_json()
+    consulta_usuario = session.query(Usuario).filter_by(login=dados_usuario.get('login')).first()
+    
+    try:
+        if consulta_usuario.login == dados_usuario.get('login'):
+            return jsonify('usuario ja existe')
+
+    except:
+        usuario = Usuario(nome=dados_usuario.get('nome'), login=dados_usuario.get('login'), password=dados_usuario.get('password'))
+        session.add(usuario)
+        session.commit()
+        return jsonify(dados_usuario)
     
 
-
-
 app.run(port=3000, host='localhost', debug=True)
+
+     
+# #ADICIONA UM NOVO USUARIO
+# @app.route('/user/new', methods=['POST'])
+# def addUser ():
+#     user = request.get_json()
+    
+#     nameAdd = user.get('name')
+#     loginAdd = user.get('login')
+#     passwordAdd = user.get('password')
+#     userAdd = Usuario(name=nameAdd,login=loginAdd,password=passwordAdd)
+   
+#     return (f'Usuario adicionado')
+        
+
+
